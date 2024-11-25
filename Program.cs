551d7001d4ed8df53848581a15ccf2d93f8831a5
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2024-11-25 12:24:59 +0100

    Solució Act4. Closes #4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ActivitatsT2AC4
+{
+    public class Codi
+    {
+        public static int[] EnterQuantity()
+        {
+            const string StartMsg = "Quants valors vols?";
+            Console.WriteLine(StartMsg);
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            int[] arrayNums = new int[quantity];
+            return arrayNums;
+        }
+        public static void Main()
+        {
+            const string QuestionMsg = "Quin valor vols introduir?";
+            try
+            {   
+                int[] arrayNums = EnterQuantity();
+                for (int i = 0; i < arrayNums.Length; i++)
+                {
+                    Console.WriteLine(QuestionMsg);
+                    arrayNums[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0;i < arrayNums.Length; i++)
+                {
+                    Console.Write(arrayNums[i]);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+            
+        }
+    }
+}
